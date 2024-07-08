5efcd10c9ebded38befa1eb8e4b4d264936da1de
albjan <albin.janzen@globalconnect.se>
2024-07-08 22:04:50 +0200

    cleanup

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -15,26 +15,11 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Make sure to setup `mapleader` and `maplocalleader` before
--- loading lazy.nvim so that mappings are correct.
--- This is also a good place to setup other settings (vim.opt)
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
-
 -- Setup lazy.nvim
 require("lazy").setup({
   spec = {
     -- import your plugins
---  { import = "plugins" },
-  {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
-  {'williamboman/mason.nvim'},
-  {'williamboman/mason-lspconfig.nvim'},
-  {'neovim/nvim-lspconfig'},
-  {'hrsh7th/cmp-nvim-lsp'},
-  {'hrsh7th/nvim-cmp'},
-  {'L3MON4D3/LuaSnip'},
-  {'mfussenegger/nvim-dap'},
-  {'mfussenegger/nvim-jdtls'},
+  { import = "plugins" },
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
@@ -43,26 +28,3 @@ require("lazy").setup({
   checker = { enabled = true },
 })
 
-local lsp_zero = require('lsp-zero')
-
-lsp_zero.on_attach(function(client, bufnr)
-  -- see :help lsp-zero-keybindings
-  -- to learn the available actions
-  lsp_zero.default_keymaps({buffer = bufnr})
-end)
-
-lsp_zero.setup()
---- if you want to know more about lsp-zero and mason.nvim
---- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guide/integrate-with-mason-nvim.md
-require('mason').setup({})
-require('mason-lspconfig').setup({
-  ensure_installed = {'jdtls'},
-  handlers = {
-    function(server_name)
-     require('lspconfig')[server_name].setup({})
-    end,
-    -- this is the "custom handler" for `jdtls`
-    -- noop is an empty function that doesn't do anything
-    jdtls = lsp_zero.noop,
-  },
-})
--- lua/config/set.lua
+++ lua/config/set.lua
@@ -19,6 +19,7 @@ vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
+vim.opt.clipboard = 'unnamedplus'
 
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
