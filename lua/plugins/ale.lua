e6fdf55ed416b10338f946db63a0a5256f4b6562
Albin Janzén <albinjanzen@gmail.com>
2025-04-08 10:08:26 +0200

    remove java and update plugins

---
+++ lua/plugins/ale.lua
@@ -0,0 +1,7 @@
+return {
+    'dense-analysis/ale',
+    config = function()
+
+
+    end
+}
--- lua/plugins/nvim-java.lua
+++
@@ -1,6 +0,0 @@
-return {
-  'nvim-java/nvim-java',
-  config = function()
-    require('lspconfig').jdtls.setup({})
-  end
-}
